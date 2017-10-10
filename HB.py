5ce3fe57884517507d53eed994462f11a9ea1314
Diego <dcotelo@collisionarts.com>
2017-10-10 11:03:21 -0300

    Primer Beta

---
+++ HB.py
@@ -0,0 +1,18 @@
+import os
+import telebot
+import re
+token = os.environ['BOT_API_TOKEN']
+bot = telebot.TeleBot(token)
+@bot.message_handler(content_types=['text'])
+def Process(message):
+    print(message)   
+    if re.findall("(?P<url>https?://[^\s]+)", message.text):
+      URL = re.findall("(?P<url>https?://[^\s]+)", message.text)
+      #bot.send_message("@get", "Url Detected", parse_mode="HTML")
+      bot.reply_to(message, u"Url Detected " + URL[0])    
+    elif message.text == "/quien" :
+      bot.reply_to(message, u"Tu vieja! @" + message.from_user.username)
+    elif message.text == "/ayuda":
+      bot.reply_to(message, u"Bot troll official H&B, originalmente destinado a salvaguardar los links compartidos la leyenda cuenta que adquirio vida pripia y comparte cosas ya discutidas en GG solo para molestar a Enrique")
+bot.polling()
+
