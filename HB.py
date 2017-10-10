f6333205bfdbbaff1afd53e16be4a3d1ed7eadfd
Diego <dcotelo@collisionarts.com>
2017-10-10 12:46:25 -0300

    Cambio menores

--- HB.py
+++ HB.py
@@ -1,8 +1,8 @@
 import os
 import telebot
 import re
-token = os.environ['BOT_API_TOKEN']
-bot = telebot.TeleBot(token)
+
+bot = telebot.TeleBot('token')
 @bot.message_handler(content_types=['text'])
 def Process(message):
     print(message)   
@@ -12,7 +12,9 @@ def Process(message):
       bot.reply_to(message, u"Url Detected " + URL[0])    
     elif message.text == "/quien" :
       bot.reply_to(message, u"Tu vieja! @" + message.from_user.username)
+    elif message.text == "/comandos" :
+      bot.reply_to(message, u"comandos disponibles /quien /ayuda /comandos")
     elif message.text == "/ayuda":
-      bot.reply_to(message, u"Bot troll official H&B, originalmente destinado a salvaguardar los links compartidos la leyenda cuenta que adquirio vida pripia y comparte cosas ya discutidas en GG solo para molestar a Enrique")
+      bot.reply_to(message, u"Bot troll official H&B, originalmente destinado a salvaguardar los links compartidos. La leyenda cuenta que adquirio vida pripia y comparte cosas ya discutidas en GG solo para molestar a Enrique")
 bot.polling()
 
